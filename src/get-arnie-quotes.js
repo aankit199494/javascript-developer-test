a305ef411eed8ae1f38f598f6a867b8e4b8ca00b
Ankit <ankit199494@icloud.com>
2025-08-25 17:23:48 +1000

    Implemented getArnieQuotes function with parallel execution and proper error handling

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,28 @@
+'use strict';
+
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+/**
+ * Maps a response to the required format while preserving quote format
+ * @param {Object} response HTTP response from httpGet
+ * @returns {Object} Formatted quote or failure object
+ */
+const mapResponse = async (url) => {
+  const { status, body } = await httpGet(url);
+  const { message } = JSON.parse(body);
+  
+  return status === 200
+    ? { 'Arnie Quote': message }
+    : { 'FAILURE': message };
+};
+
+/**
+ * Executes a HTTP GET request on each of the URLs, transforms each of the HTTP responses returns the results.
+ * @param {string[]} urls The urls to be requested
+ * @return {Promise} A promise which resolves to a results array
+ */
+const getArnieQuotes = (urls) => {
+  return Promise.all(urls.map(mapResponse));
 };
 
 module.exports = {
